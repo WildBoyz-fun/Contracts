665cbd0acf8ebc959a2a0e2d760607ecc3e5c5fc
inkyu.han <amave99@gmail.com>
2025-03-22 22:13:29 +0900

    adjust 10 ether = 1 billion token

--- contracts/BondingCurveSwap.sol
+++ contracts/BondingCurveSwap.sol
@@ -10,13 +10,23 @@ contract BondingCurveSwap is BancorFormula, MaxGasPrice {
     event Debug(string message, uint256 value);
 
     IERC20 private _token;
-    uint256 private _scale = 10**18;
-    uint256 private _tokenSupply = 1*_scale;
-    uint256 private _depositBalance = 1*_scale;
+    uint256 private constant SCALE = 10**18;
+    uint256 private constant INITIAL_TOKEN_SUPPLY = 3_690_287_251_496_824_536_722_139_764;
+    uint256 private constant INITIAL_DEPOSIT_BALANCE = 1*SCALE;
+    uint256 private _tokenSupply = INITIAL_TOKEN_SUPPLY;
+    uint256 private _depositBalance = INITIAL_DEPOSIT_BALANCE;
     uint256 private _reserveRatio = 100000; // 10%
 
     constructor(address tokenAddress) {
         _token = IERC20(tokenAddress);
+    }
+
+    function getTokenSupply() public view returns (uint256) {
+        return _tokenSupply - INITIAL_TOKEN_SUPPLY;
+    }
+
+    function getDepositBalance() public view returns (uint256) {
+        return _depositBalance - INITIAL_DEPOSIT_BALANCE;
     }
 
     function calculatePurchaseReturn(uint256 deposit) public view returns (uint256) {
