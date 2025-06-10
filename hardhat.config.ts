7b076b439291dffc6ef647d5014a46dfafa409fb
happyDev00 <climbtops@gmail.com>
2025-06-10 23:52:29 +0900

    add deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -31,6 +36,10 @@ const config: HardhatUserConfig = {
     },
     localhost: {
       initialBaseFeePerGas: 0,
+    },
+    monad_testnet: {
+      url: process.env.MONAD_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     }
   }
 };
