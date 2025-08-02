62b4994438e82eea0dd45f86a4f5f19bbc4e95db
bsnamwincl <46341567+Nam-Busung@users.noreply.github.com>
2025-08-02 20:50:10 +0900

    Update hardhat configuration and package dependencies; add dotenv and @openzeppelin/hardhat-upgrades, and enhance OwnerGroup module with environment variable validation.

--- ignition/modules/OwnerGroup.ts
+++ ignition/modules/OwnerGroup.ts
@@ -10,6 +10,10 @@ const OwnerGroupModule = buildModule("OwnerGroupModule", (m) => {
   const ownerAddress = process.env.OWNER_ADDRESS;
   console.log(`ownerAddress : ${ownerAddress}`);
   
+  if (!ownerAddress) {
+    throw new Error("OWNER_ADDRESS environment variable is not set");
+  }
+  
   const ownerGroupContract = m.contract("OwnerGroupContract", [[ownerAddress]]);
 
   console.log(`OwnerGroupContract: ${ownerGroupContract}`)
